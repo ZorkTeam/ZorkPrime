161912afb06b6556b0be387e8e3ca8610f5a635c
Unknown <tyther9@nettally.com>
2017-07-05 23:05:23 -0400

    Added navigation and action processing to engine. (Enjoy the snarky bad entries!)

--- gameengine.py
+++ gameengine.py
@@ -6,14 +6,14 @@ class GameEngine(object):
     def __init__(self):
         self.locationx = 0
         self.locationy = 0
-        self.action = ''
+        self.action = ' '
         self.location_description = ''
         self.enemy = None
         self.enemy_description = ''
     
     
     def main_loop(self):
-        while self.action != 'Quit':
+        while self.action != 'QUIT':
             os.system('cls' if os.name == 'nt' else 'clear')
                         
             self.read_location()
@@ -28,6 +28,7 @@ class GameEngine(object):
             print("What do you want to do? ")
             self.action = raw_input()
             
+            self.action = self.action.strip().upper()
             self.process_action()
             
     def read_location(self):
@@ -50,6 +51,64 @@ class GameEngine(object):
         
 ##        if ((self.locationx >= 0 and self.locationx <= 5) and
 ##            (self.locationy >= 0 and self.locationy <= 8)):
+
+    def process_action(self):
+        if "QUIT" in self.action:
+            return  ## Nothing to do
+        
+        if "GO " in self.action:
+            self.navigate()
             
+        elif "SEARCH" in self.action:
+            self.search_area()
+        
+        elif "USE " in self.action:
+            self.satchel_action()
+        
+        else:
+            print("I'm confused. Do what now?")
+            raw_input("\nPress Enter to continue.")
+        
+        
+    def navigate(self):
+        if "EAST" in self.action:
+            self.locationx += 1
             
+            if self.locationx > 17:
+                self.locationx = 17
+                print("I can't go that way. Can't you read?")
+                raw_input("\nPress Enter to continue.")
             
+        elif "WEST" in self.action:
+            self.locationx -= 1
+            
+            if self.locationx < 0:
+                self.locationx = 0
+                print("I can't go that way. Can't you read?")
+                raw_input("\nPress Enter to continue.")
+                
+        elif "SOUTH" in self.action:
+            self.locationy += 1
+            
+            if self.locationy > 17:
+                self.locationy = 17
+                print("I can't go that way. Can't you read?")
+                raw_input("\nPress Enter to continue.")
+                
+        elif "NORTH" in self.action:
+            self.locationy -= 1
+            
+            if self.locationy < 0:
+                self.locationy = 0
+                print("I can't go that way. Can't you read?")
+                raw_input("\nPress Enter to continue.")
+        else:
+            print("Go where? I don't think so. Sober up!")
+            raw_input("\nPress Enter to continue.")
+        
+    def search_area(self):
+        return
+        
+    def satchel_action(self):
+        return
+    
