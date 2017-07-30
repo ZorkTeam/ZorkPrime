b9f667540f89d957af7e6e48def42f9d6ac9de66
Unknown <tyther9@nettally.com>
2017-07-30 16:48:32 -0400

    Added enemy.txt so no hard-coded enemies (right now just 4 add more if you want)
    started use method.

--- gameengine.py
+++ gameengine.py
@@ -5,27 +5,29 @@ import time
 
 class GameEngine(object):
     def __init__(self):
-        self.locationx = 0
-        self.locationy = 0
+        self.locationx = 5
+        self.locationy = 17
         self.action = ' '
         self.location_description = ''
         self.enemy = None
-        self.enemy_description = ''
-        self.items = []
+        self.items = [("Rusty Knife", 1, 5)]
         self.exititemx = random.randint(0,17)
         self.exititemy = random.randint(0,17)
+        self.playerhitpoints = 50
     
     def main_loop(self):
         while self.action != 'QUIT':
             os.system('cls' if os.name == 'nt' else 'clear')
-                        
             self.read_location()
             
             print(self.location_description)
             
             self.generate_enemy()
             
-            print(self.enemy_description)
+            if self.enemy != None:
+                print("You have encountered a {0}.\n\n".format(self.enemy[0]))
+                print("{0}\n".format(self.enemy[3]))
+            
             print("\n")
             
             print("What do you want to do? ")
@@ -33,7 +35,8 @@ class GameEngine(object):
             
             self.action = self.action.strip().upper()
             self.process_action()
-            
+
+
     def read_location(self):
         with open("map.txt", "r") as themap:
             for line in themap:
@@ -43,17 +46,24 @@ class GameEngine(object):
 
     def generate_enemy(self):
         possible = random.randint(1,10)
-        
-        ## no enemy
         if possible not in [2,5,10]:
             self.enemy = None
             self.enemy_description = ''
             return 
-        
-        self.enemy_description = "An enemy was encountered"
-        
-##        if ((self.locationx >= 0 and self.locationx <= 5) and
-##            (self.locationy >= 0 and self.locationy <= 8)):
+
+        else:
+            which = random.randint(1,4)
+
+            with open("enemy.txt", "r") as enemies:
+                for line in enemies:
+                    if line == "[{0}]\n".format(which):
+                        name = enemies.next().strip()
+                        hitpoints = int(enemies.next())
+                        deals = int(enemies.next())
+                        description = enemies.next()
+                        
+                        self.enemy = name, hitpoints, deals, description
+                        break
 
     def process_action(self):
         if "QUIT" in self.action:
@@ -114,19 +124,20 @@ class GameEngine(object):
         new_item = self.generate_item()
 
         print("After a careful search of the area, you've found...")
-        if new_item:
+        if new_item != None:
             print("\t\t" + new_item[0])
             self.items.append(new_item)
             print("You added the " + new_item[0] + " to your satchel.")
         else:
             print("\t\tNothing!")
-
+            
+        raw_input("\nPress Enter to continue.")
 
     def generate_item(self):
-        item_chance = random.randint(500)
+        item_chance = random.randint(1, 500)
 
         if self.locationx == self.exititemx and self.locationy == self.exititemy:
-            return ("Briar killer", -2, 0)
+            return ("Wand of Solomon", -2, 0)
 
         if item_chance >= 400:
             return None
@@ -156,5 +167,9 @@ class GameEngine(object):
             return ('ULTIMATE HAMMER OF GOD', 0, 100)
 
     def satchel_action(self):
+        usewhat = self.action.upper().strip().replace("USE ","")
+        
+        
+        
         return
     
