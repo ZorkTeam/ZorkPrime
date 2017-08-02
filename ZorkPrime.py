99997ca320e16107b89268cb7c0b6985217e2902
Robert Zink <robertzinkfl@gmail.com>
2017-08-02 19:24:45 -0400

    Added comments to all functions

--- ZorkPrime.py
+++ ZorkPrime.py
@@ -1,4 +1,20 @@
+''' Module ZorkPrime.py '''
+
 from gameengine import GameEngine
+
+"""
+ZorkPrime
+
+Contributors:
+    Richard Hamm    (reh14c@my.fsu.edu)
+    Jordan Harlow   (jth14b@my.fsu.edu)
+    Robert Zink     (rjz11@my.fsu.edu)
+
+CIS4930 - Python Programming
+Summer 2017
+4 August 2017
+ZorkPrime.py
+"""
 
 g = GameEngine()
 g.main_loop()
